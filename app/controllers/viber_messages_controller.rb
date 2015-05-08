ff65a48dea7455512208c746f74480c12417edcf
Jonathan Warrick <jonathan.warrick@gmail.com>
2015-05-07 23:20:50 -0400

    Super basic working.

--- app/controllers/viber_messages_controller.rb
+++ app/controllers/viber_messages_controller.rb
@@ -2,4 +2,7 @@ class ViberMessagesController < ApplicationController
 	def index
 		@message_counts = User.all.map{|user| [user.name, user.viber_messages.where(:room_id => Room.find_by_room_name("Anything but FF, including E - messages.CSV").id).count] }.select{|x| x[1] > 0}
 	end
+	def search
+		@search_results = ViberMessage.where('message_text LIKE ?', "%#{params[:q]}%")
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'viber_messages#index'
+  get  'viber_messages/search' => 'viber_messages#search'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
